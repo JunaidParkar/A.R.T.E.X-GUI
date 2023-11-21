9ae6816658b6e6c334e0d204809dc30ad697d27c
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2023-11-21 23:28:18 +0530

    Optimised JS and re-programmed news and fixed bugs

--- apps/cmd/script.js
+++ apps/cmd/script.js
@@ -8,5 +8,7 @@ document.getElementById("cmdContainer").addEventListener("click", () => {
 
 document.getElementById("cmdInp").addEventListener("input", () => {
   let contentWidth = document.getElementById("cmdInp").scrollWidth;
+  let contentheight = document.getElementById("cmdInp").scrollHeight;
   document.getElementById("cmdInp").style.width = contentWidth + "px";
+  document.getElementById("cmdInp").style.height = contentheight + "px";
 });
